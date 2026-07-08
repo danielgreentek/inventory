c79b5afd0dda111e401178dc4ffcd5cb5a7f95af
danielgreentek <daniel.hutabarat@greentekindonesia.co.id>
2026-07-08 15:24:32 +0700

    fix: use standard PrismaClient instead of Neon HTTP adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaNeonHttp } from '@prisma/adapter-neon'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL!
-  const adapter = new PrismaNeonHttp(connectionString, {})
-  return new PrismaClient({ adapter })
-}
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
